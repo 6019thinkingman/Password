2e312ff9297d52c30c4d7e47246c98b21aa30d8d
6019thinkingman <99737495+6019thinkingman@users.noreply.github.com>
2022-02-15 16:42:57 +0300

    passwordassiment

---
+++ password_assigment.py
@@ -0,0 +1,9 @@
+want_name = "Ahmet"
+passwordkey = "ah123met"
+name = input("lütfen isminizi giriniz :")
+if name == want_name:
+    print(f"Merhaba {name} şifren {passwordkey} dir.")
+else:
+    print(f"Merhaba {name} daha sonra görüşmek dileğiyle...")
+    
+
